a709f9f707132d83fa34e55d83c73bb4550d0bb3
Allkrain <aukra25@yandex.ru>
2019-04-05 14:39:49 +0300

    fixes

--- app/components/venezia/venezia.js
+++ app/components/venezia/venezia.js
@@ -11,33 +11,31 @@ import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugin
 const $ = window.$;
 
 export default function venezia() {
-  if (!window.isMobile) {
-    const controller = new ScrollMagic.Controller();
+  const controller = new ScrollMagic.Controller();
 
-    const anim = new TimelineMax()
-      .add([
-        TweenMax.fromTo('.venezia-applique__mac', 1, {
-          y: 100,
-        }, {
-          y: -100,
-          ease: Power0.easeNone,
-        }),
-        TweenMax.fromTo('.venezia-applique__screen', 1, {
-          y: 200,
-        }, {
-          y: -200,
-          ease: Power0.easeNone,
-        }),
-      ]);
+  const anim = new TimelineMax()
+    .add([
+      TweenMax.fromTo('.venezia-applique__mac', 1, {
+        y: 50,
+      }, {
+        y: -50,
+        ease: Power0.easeNone,
+      }),
+      TweenMax.fromTo('.venezia-applique__screen', 1, {
+        y: 150,
+      }, {
+        y: -150,
+        ease: Power0.easeNone,
+      }),
+    ]);
 
-    const scene = new ScrollMagic
-      .Scene({
-        triggerElement: '.venezia-applique',
-        duration: $('.venezia-applique').outerHeight(),
-      })
-      .setTween(anim)
-      .addTo(controller);
-  }
+  const scene = new ScrollMagic
+    .Scene({
+      triggerElement: '.venezia-applique',
+      duration: $('.venezia-applique').outerHeight(),
+    })
+    .setTween(anim)
+    .addTo(controller);
 }
 
 /* eslint-enable */
