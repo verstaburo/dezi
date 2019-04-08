b1b873d232fbb28c99b029d1aebe5be5b14e371e
Allkrain <aukra25@yandex.ru>
2019-04-08 19:12:03 +0300

    secret place

--- app/components/venezia/venezia.js
+++ app/components/venezia/venezia.js
@@ -13,35 +13,37 @@ const $ = window.$;
 export default function venezia() {
   const controller = new ScrollMagic.Controller();
 
-  const anim = new TimelineMax()
-    .add([
-      TweenMax.fromTo('.venezia-applique__mac', 1, {
-        y: 50,
-      }, {
-        y: -50,
-        ease: Power0.easeNone,
-      }),
-      TweenMax.fromTo('.venezia-applique__describe', 1, {
-        y: 50,
-      }, {
-        y: -50,
-        ease: Power0.easeNone,
-      }),
-      TweenMax.fromTo('.venezia-applique__screen', 1, {
-        y: '15%',
-      }, {
-        y: '-15%',
-        ease: Power0.easeNone,
-      }),
-    ]);
+  if ($('.venezia-applique').length > 0) {
+    const anim = new TimelineMax()
+      .add([
+        TweenMax.fromTo('.venezia-applique__mac', 1, {
+          y: 50,
+        }, {
+          y: -50,
+          ease: Power0.easeNone,
+        }),
+        TweenMax.fromTo('.venezia-applique__describe', 1, {
+          y: 50,
+        }, {
+          y: -50,
+          ease: Power0.easeNone,
+        }),
+        TweenMax.fromTo('.venezia-applique__screen', 1, {
+          y: '15%',
+        }, {
+          y: '-15%',
+          ease: Power0.easeNone,
+        }),
+      ]);
 
-  const scene = new ScrollMagic
-    .Scene({
-      triggerElement: '.venezia-applique',
-      duration: $(window).height(),
-    })
-    .setTween(anim)
-    .addTo(controller);
+    const scene = new ScrollMagic
+      .Scene({
+        triggerElement: '.venezia-applique',
+        duration: $(window).height(),
+      })
+      .setTween(anim)
+      .addTo(controller);
+  }
 }
 
 /* eslint-enable */
