8528767330ca98fe7387ad75d2fc9bfd8648b87f
Allkrain <aukra25@yandex.ru>
2019-08-28 17:24:41 +0300

    fixes

--- app/pages/services/services.js
+++ app/pages/services/services.js
@@ -5,31 +5,45 @@ const $ = window.$;
 
 export default function watchAnimation() {
   function watchAnim() {
-    watch();
-    const controller = new ScrollMagic.Controller();
-    const scene = new ScrollMagic
-      .Scene({
-        triggerElement: '.js-watch-3',
-      })
-      .addTo(controller);
-    scene.on('enter', () => {
-      const wtch = document.getElementById('watch');
-      const see = wtch.watch;
-      $('.js-watch-3').addClass('is-animate');
-      setTimeout(() => {
-        $('.js-watch-1').addClass('is-animate');
-        see.play();
-        setTimeout(() => {
-          $('.js-watch-2').addClass('is-animate');
-        }, 5000);
-        scene.off('enter');
-      }, 1500);
-    });
+    const wtch = document.getElementById('watch');
+    const trigger = document.querySelectorAll('.js-watch-3');
+    if (trigger.length) {
+      if (wtch) {
+        watch();
+      }
+      const controller = new ScrollMagic.Controller();
+      const scene = new ScrollMagic
+        .Scene({
+          triggerElement: '.js-watch-3',
+        })
+        .addTo(controller);
+      scene.on('enter', () => {
+        if (wtch) {
+          const see = wtch.watch;
+          $('.js-watch-3').addClass('is-animate');
+          setTimeout(() => {
+            $('.js-watch-1').addClass('is-animate');
+            see.play();
+            setTimeout(() => {
+              $('.js-watch-2').addClass('is-animate');
+            }, 5000);
+            scene.off('enter');
+          }, 1500);
+        } else {
+          $('.js-watch-3').addClass('is-animate');
+          setTimeout(() => {
+            $('.js-watch-1').addClass('is-animate');
+            setTimeout(() => {
+              $('.js-watch-2').addClass('is-animate');
+            }, 600);
+            scene.off('enter');
+          }, 1500);
+        }
+      });
+    }
   }
 
-  if (document.getElementById('watch')) {
-    watchAnim();
-  }
+  watchAnim();
 
   window.globalFunctions.watchAnim = watchAnim;
 }
