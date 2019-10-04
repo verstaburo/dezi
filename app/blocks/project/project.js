697148fb190ff8f7f0a52200c2192d8a1ff8661f
Allkrain <aukra25@yandex.ru>
2019-10-04 14:53:22 +0300

    fixes

--- app/blocks/project/project.js
+++ app/blocks/project/project.js
@@ -4,18 +4,18 @@ export default function bgvideo() {
   const breakpoints = window.globalOptions.sizes;
 
   function videoActivation() {
-    const wH = $(window).height();
+    // const wH = $(window).height();
     const wW = $(window).width();
     $('.js-video').each((i, el) => {
-      const wrapper = $(el).closest('[data-video-container]');
-      const top = $(wrapper).offset().top;
-      const vH = $(wrapper).outerHeight();
-      const bottom = top + vH;
+      // const wrapper = $(el).closest('[data-video-container]');
+      // const top = $(wrapper).offset().top;
+      // const vH = $(wrapper).outerHeight();
+      // const bottom = top + vH;
       if (wW < breakpoints.md) {
         $(el).removeAttr('autoplay');
         $(el)[0].pause();
-      } else if (bottom <= 0 || top >= wH) {
-        $(el)[0].pause();
+        // } else if (bottom <= 0 || top >= wH) {
+        //   $(el)[0].pause();
       } else {
         $(el).attr('preload', 'auto');
         $(el).attr('autoplay', 'autoplay');
@@ -26,5 +26,5 @@ export default function bgvideo() {
 
   videoActivation();
 
-  $(window).on('scroll', videoActivation);
+  $(window).on('resize', videoActivation);
 }
