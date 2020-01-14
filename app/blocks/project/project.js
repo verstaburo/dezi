4b5f8efce94f23a7c5c65b8d9d8f7e3ee056db08
Allkrain <aukra25@yandex.ru>
2020-01-14 12:43:29 +0300

    video phases

--- app/blocks/project/project.js
+++ app/blocks/project/project.js
@@ -8,25 +8,67 @@ export default function bgvideo() {
     const wW = $(window).width();
     const wH = $(window).height();
     $('.js-video').each((i, el) => {
-      if (wW < breakpoints.md) {
-        $(el).removeAttr('autoplay');
-        $(el)[0].pause();
-      } else {
-        const top = $(el).offset().top;
-        const vH = $(el).outerHeight();
-        const bottom = top + vH;
-        $(el).attr('preload', 'auto');
-        $(el).attr('autoplay', 'autoplay');
-        if ((bottom >= sT && bottom <= (sT + wH)) || (top >= sT && (top <= (sT + wH)))) {
-          $(el)[0].play();
-        } else {
+      const container = $(el).closest('[data-video-container]');
+      if (container.length === 0 || $(container).hasClass('is-loaded')) {
+        if (wW < breakpoints.md) {
+          $(el).removeAttr('autoplay');
           $(el)[0].pause();
+          $(el).removeClass('is-play');
+        } else {
+          const top = $(el).offset().top;
+          const vH = $(el).outerHeight();
+          const bottom = top + vH;
+          $(el).attr('preload', 'auto');
+          $(el).attr('autoplay', 'autoplay');
+          if ((bottom >= sT && bottom <= (sT + wH)) || (top >= sT && (top <= (sT + wH)))) {
+            $(el)[0].play();
+            $(el).addClass('is-play');
+          } else {
+            $(el)[0].pause();
+            $(el).removeClass('is-play');
+          }
         }
+      } else if (container.length > 0 && $(el)[0].readyState !== 4) {
+        $(container).addClass('is-loading');
+      } else if (container.length > 0 && $(el)[0].readyState === 4) {
+        $(container).removeClass('is-loading').addClass('is-loaded');
       }
     });
   }
 
   videoActivation();
+
+  // $('.js-video').on('loadstart, progress', (evt) => {
+  //   const self = evt.currentTarget;
+  //   const wrapper = $(self).closest('[data-video-container]');
+  //   if (wrapper.length > 0) {
+  //     $(wrapper).addClass('is-loading');
+  //   }
+  // });
+
+  $('.js-video').on('canplaythrough', (evt) => {
+    const self = evt.currentTarget;
+    const wrapper = $(self).closest('[data-video-container]');
+    if (wrapper.length > 0) {
+      $(wrapper).removeClass('is-loading');
+      $(wrapper).addClass('is-loaded');
+      videoActivation();
+    }
+  });
+
+  $(document).on('click', '.js-video', (evt) => {
+    const video = evt.currentTarget;
+    const wW = $(window).width();
+    if (wW < breakpoints.md) {
+      if ($(video).hasClass('is-play')) {
+        $(video).removeClass('is-play');
+        video.pause();
+      } else {
+        video.play();
+        $(video).addClass('is-play');
+      }
+    }
+  });
 
   $(window).on('resize', videoActivation);
 
